89dcd8c3bb0a5d9dc751d482287770222ce254a8
vikram23102003 <vikramkashyap23vk@gmail.com>
2023-05-15 20:14:57 +0530

    Create javas.js

---
+++ javas.js
@@ -0,0 +1,43 @@
+/*let age = 16 ; 
+console.log( "You can" ,(age>18 || age == 18 )? "drive" : "not drive");*/
+
+/*let Obj = {
+    Maths : 15,
+    Thermo : 10,
+    MTT : 12,
+    MOS : 10
+}
+
+for (let i in Obj){
+    console.log(`Marks of ${i} is ${Obj[i]}`);
+
+} */
+/*let Avg5 = (a,b,c,d,e)=>{
+    return (a+b+c+d+e)/5;
+}
+
+let i = Avg5(2,3,4,5,6);
+console.log(i)*/
+/*let i = prompt("Enter Passward");
+while(i != 6){
+    console.log("Try Again");
+    i = prompt("Enter Right Passward");
+}*/
+/* let str = "Vicky";
+console.log(str.toLowerCase());
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.toLowerCase()); */
+
+/* str = "you give me rs 1000";
+console.log(str.slice(0,14)); */
+
+/* let str = "Vicky"
+console.log(str.replace("y","u")); */
+
+// let arr = [ 1, 2, 3, 4, 5, 6, 7]
+// let a = 2000000;
+// arr.push(a)
+// console.log(arr);
+
